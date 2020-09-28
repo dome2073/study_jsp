520278d376ac2d9cb76684035c30ecf6b937ad01
dome2073 <wlgns2073@gmail.com>
2020-09-28 14:50:30 +0900

    7. 작성한것을 UserDao로 변경

--- jdbcTest/src/dao/UserDao.java
+++ jdbcTest/src/dao/UserDao.java
@@ -12,38 +12,43 @@ import model.User;
 
 public class UserDao {
 	
-	private static UserDao instance = new UserDao();
+	private static UserDao userdao;
+	
+	private static Connection conn;
+	private static PreparedStatement pstmt;
+	private static ResultSet rs;
+	
+	private static String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+	private static String DB_USER = "student";
+	private static String DB_PASSWORD = "1234";
 	
 	//싱글턴 패턴
 	//생성자를 private로
 	private UserDao() {
-		
+		try {
+			Class.forName(DRIVER);
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	public static UserDao getInstance(){
-		return instance;
+		if(userdao == null) {
+			userdao = new UserDao();
+		}
+		return userdao;
 	}
 	
 	public static List<User> selectAllUsers() {
 		// TODO Auto-generated method stub
 		List<User> userList = new ArrayList<User>();
-		// TODO Auto-generated method stub
-		String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String DB_USER = "student";
-		String DB_PASSWORD = "1234";
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
+		
 		String query = "select * from user_info"; // 실행할 쿼리
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
+			
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 설정한다.
 			pstmt = conn.prepareStatement(query); // Statement를 가져온다.
-
-
 			rs = pstmt.executeQuery(); // SQL문을 실행한다.
 
 			while (rs.next()) {
@@ -85,21 +90,13 @@ public class UserDao {
 	}
 	// 5. 객체u에 저장된 user_id와 동일한 행의 정보를 delete하는메서드
 	public static int deleteUser(String user_id) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-		String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String DB_USER = "student";
-		String DB_PASSWORD = "1234";
+		
 		int SQLSTATUS = 0;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		String query = "delete from user_info where user_id = ?"; // 실행할 쿼리
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
 
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 설정한다.
 			pstmt = conn.prepareStatement(query); // Statement를 가져온다.
@@ -130,19 +127,12 @@ public class UserDao {
 	// 4. user_id를 매개변수로 받아 user_info테이블에서 해당 사용자의정보를 가져오는 selectUser()메서드
 	public  static User selectUser(String userId) {
 		User user = new User();
-		// TODO Auto-generated method stub
-		String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String DB_USER = "student";
-		String DB_PASSWORD = "1234";
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		
 
 		String query = "select * from user_info where user_id = ?"; // 실행할 쿼리
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
 
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 설정한다.
 			pstmt = conn.prepareStatement(query); // Statement를 가져온다.
@@ -191,20 +181,14 @@ public class UserDao {
 	// 3. User객체를 매개변수로 받아 user_info테이블에서 해당 사용자의 정보를 update하는 updateUser()메서드를 작성
 	public static int updateUser(User u) {
 		// TODO Auto-generated method stub
-		String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String DB_USER = "student";
-		String DB_PASSWORD = "1234";
+
 		int SQLSTATUS = 0;
 
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		String query = "update user_info set user_id = '변경' where user_id = ?"; // 실행할 쿼리
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-
+			
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 설정한다.
 			pstmt = conn.prepareStatement(query); // Statement를 가져온다.
 
@@ -234,19 +218,13 @@ public class UserDao {
 	// update , insert - executeUpdate()
 	// 2. User 객체를 매갠수로 받아 DB의 user_info테이블에 insert하는 insertUser()메서드
 	public int insertUser(User u) {
-		String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-		String DB_USER = "student";
-		String DB_PASSWORD = "1234";
+		
 		int SQLSTATUS = 0;
 
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
 		String query = "insert into user_info (USER_ID, PASSWORD, NAME, EMAIL) values(?,?,?,?)"; // 실행할 쿼리
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
 
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD); // 데이터베이스의 연결을 설정한다.
 			pstmt = conn.prepareStatement(query); // Statement를 가져온다.
